31d22fd5b44f49eb0a7bc5ad8935204dcecb6c73
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 09:04:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerificarNivel from './components/VerificarNivel';
+
+function App() {
+  return (
+    <div className="App">
+      <VerificarNivel nivel="intermediário" />
+    </div>
+  );
+}
+
+export default App;
